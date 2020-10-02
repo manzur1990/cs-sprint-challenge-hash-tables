0924a989fb844eb31fc0927501dde704bb8d6f34
JM <manzur-1990@hotmail.com>
2020-10-02 11:59:59 -0500

    Update ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,22 @@
 def has_negatives(a):
     """
     YOUR CODE HERE
+    1. set up a result list
+    2. set up a hash table
+    3. loop through list _a_
+        a. set up posetive number int in the hash table
+        b. if number not 0 or in dictionary
+            a. append to dictionary
     """
     # Your code here
+    result = []
+    ht = {}
+
+    for pn in a:
+        ht[pn] = pn
+
+        if pn != 0 and -pn in ht:
+            result.append(abs(pn))
 
     return result
 
